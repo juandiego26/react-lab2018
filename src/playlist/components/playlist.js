8c6085e0d1658d1d3d82e12f27ee6202a58228fa
Juan Diego <silgajuandiego@gmail.com>
2018-10-02 15:31:54 -0500

    [Add] Composisción de componentes

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,20 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Media from './media.js'
 import './playlist.css'
+import Play from '../../icons/components/play'
 
-class Playlist extends Component {
-  render() {
-    const playlist = this.props.data.categories[0].playlist
-    return (
-      <div>
-        {
-          playlist.map((item) => {
-            return <Media {...item} key={item.id}/>
-          })
-        }
-      </div>
-    )
-  }
+function Playlist(props) {
+  const playlist = props.data.categories[0].playlist
+  console.log(props.data)
+  return (
+    <div className="Playlist">
+      <Play
+        size={100}
+        color="red"
+      />
+      {
+        playlist.map((item) => {
+          return <Media {...item} key={item.id}/>
+        })
+      }
+    </div>
+  )
 }
 
 export default Playlist
