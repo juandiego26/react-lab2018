38020318b147c17a3f588eba62b1a348a7b91f5f
Juan Diego <silgajuandiego@gmail.com>
2018-10-04 07:43:39 -0500

    [Add] Clase No.24 Add handleErrors en React y formularios

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -4,11 +4,12 @@ import Categories from '../../categories/components/categories'
 import Related from '../components/related'
 import ModalContainer from '../../widgets/containers/modal'
 import Modal from '../../widgets/components/modal'
+import HandleError from '../../error/containers/handle-error'
 
 class Home extends Component {
 
   state = {
-    modalVisible: false // inicamos el estado del modal para que se muestre al inicio
+    modalVisible: false, // inicamos el estado del modal para que se muestre al inicio
   }
 
   handleOpenModal = () => {
@@ -22,25 +23,28 @@ class Home extends Component {
       modalVisible: false // seteamos el estado del modal
     })
   }
+
   render() {
     return(
-      <HomeLayout>
-        <Related />
-        <Categories
-          categories={this.props.data.categories}
-          handleOpenModal={this.handleOpenModal}
-        />
-        {
-          this.state.modalVisible && // operador ternario para ver si se rederiza el modal
-          <ModalContainer>
-            <Modal
-              handleClick={this.handleCloseModal}
-            >
-            <h1>Esto es un portal</h1>
-            </Modal>
-          </ModalContainer>
-        }
-      </HomeLayout>
+      <HandleError>
+        <HomeLayout>
+          <Related />
+          <Categories
+            categories={this.props.data.categories}
+            handleOpenModal={this.handleOpenModal}
+          />
+          {
+            this.state.modalVisible && // operador ternario para ver si se rederiza el modal
+            <ModalContainer>
+              <Modal
+                handleClick={this.handleCloseModal}
+              >
+              <h1>Esto es un portal</h1>
+              </Modal>
+            </ModalContainer>
+          }
+        </HomeLayout>
+      </HandleError>
     )
   }
 }
