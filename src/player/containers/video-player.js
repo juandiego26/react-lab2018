885295c9ef3f0264c55718312171878c40ffca46
Juan Diego <silgajuandiego@gmail.com>
2018-10-05 20:46:02 -0500

    [Add] Clase No.29 agregando la lógica de Play/pause y la duración del video

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,16 +2,53 @@ import React, { Component } from 'react'
 import VideoPlayerLayout from '../components/video-player-layout'
 import Video from '../components/video'
 import Title from '../components/title'
+import PlayPause from '../components/play-pause'
+import Timer from '../components/timer'
+import Controls from '../components/video-player-controls'
 
 class VideoPlayer extends Component {
+  state = {
+    pause: true,
+    duration: 0
+  }
+  togglePlay = (event) => {
+    this.setState({
+      pause: !this.state.pause
+    })
+  }
+
+  componentDidMount() { // referencia para verificar en que estado estamos y cambiar el estado que esta en pausa
+    this.setState({
+      pause: (!this.props.autoplay)
+    })
+  }
+
+  handleLoadedMetadata = event => {
+    this.video = event.target
+    this.setState({
+      duration: this.video.duration
+    })
+  }
+
   render() {
     return (
       <VideoPlayerLayout>
         <Title
           title="Esto es un video chido!"
         />
+        <Controls>
+          <PlayPause
+            pause={this.state.pause}
+            handleClick={this.togglePlay}
+          />
+          <Timer
+            duration={this.state.duration}
+          />
+        </Controls>
         <Video
-          autoplay={true}
+          autoplay={this.props.autoplay}
+          pause={this.state.pause}
+          handleLoadedMetadata={this.handleLoadedMetadata}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
         />
       </VideoPlayerLayout>
