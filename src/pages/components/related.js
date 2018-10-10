ca42a2a3fd419f63a212be7a27875fc1c243c682
Juan Diego <silgajuandiego@gmail.com>
2018-10-10 14:55:17 -0500

    [Add] Optimizando imagen Base64 para produccion

--- src/pages/components/related.js
+++ src/pages/components/related.js
@@ -5,7 +5,8 @@ import './related.css'
 function Related(props) {
   return (
     <div className="Related">
-      <img src={logo} width={250}/>
+      <img src={logo} width={250}/> {/* Para desarrollo*/}
+      {/* <img src="./dist/images/logo.c06f5e5a2ae47006c0b77be4de6a2077.png" width={250}/> */}
     </div>
   )
 }
